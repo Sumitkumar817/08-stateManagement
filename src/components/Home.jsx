0e168c03f074025a7712f5d47d3f736475da9a52
Sumit Kumar <sumitkumar12107@gmail.com>
2026-07-05 16:08:42 +0530

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+function Home() {
+
+    const [first, setfirst] = useState(0)
+
+
+    const noAdd = (a) => {
+        setfirst(first + a)
+    }
+
+    const noSub = (a) => {
+        setfirst(first-a)
+    }
+
+
+
+
+  return (
+
+
+    <div className="flex flex-col w-full items-center justify-center mt-10">
+        <div className="m-4 p-2 text-2xl">
+            Total count={first}
+        </div>
+
+      <div>
+        <button onClick={(e)=>{
+        noAdd(5)
+      }} className="m-4 p-2 w-[150px] bg-pink-700 rounded-full text-white text-2xl">
+        Add 5
+      </button>
+
+      <button onClick={(e)=>{
+        noSub(5)
+      }} className="m-4 p-2 w-[150px] bg-pink-700 rounded-full text-white text-2xl">
+        Minus 5
+      </button>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
